f6d73915c532fa66a1891ed6a52357ebdc543089
NatanSchiavon <106755155+NatanSchiavon@users.noreply.github.com>
2023-10-27 08:52:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
+}
+document.querySelectorAll('.tecla')
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+listadeTeclas[0].onclick=tocaSomPom;
+
+while(Contador <9) {
+    listadeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador +1;
+}
+
