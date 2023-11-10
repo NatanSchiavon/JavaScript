b772d5801fd1f399bb11ffc2f4cf960c83fdf79d
NatanSchiavon <106755155+NatanSchiavon@users.noreply.github.com>
2023-11-10 08:57:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-function tocaSomPom(idElementoAudio){
-    document.querySelector(idElementoAudio).play('');
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio).play('');
+}  
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }
+
+    else{
+alert('elemento não encontrado')
+
 }
+
 document.querySelectorAll('.tecla')
 
 const listadeTeclas = document.querySelectorAll('.tecla');
 
 listadeTeclas[0].onclick=tocaSomPom;
 
-while(Contador <9) {
-    listadeTeclas[contador].onclick = tocaSomPom;
+for (contador = 0; contador < listadeTeclas.length; contador++) {
+   
+    tecla.onclick= function(){
+        TocaSom(idAudio);
+    };
 
-    contador = contador +1;
+    tecla.onkeydown = function() {
+        if( evento.code === 'space'|| evento.code === 'enter'){
+            tecla.classList.add('ativa');
+        }
+       
+
+tecla.classList.add('ativa');
+    }
+    
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('atvia');
+    }
+
+   const tecla= listadeTeclas[contador];
+   
+   const idAudiov = `som_${instrumento}`;
 }
-
+ 
